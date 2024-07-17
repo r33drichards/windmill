f59b87f88bae63f7c922ddd58fc299410ae5274a
rw <rw@jjk.is>
2024-07-17 14:53:38 -0700

    shrug

--- configuration.nix
+++ configuration.nix
@@ -6,8 +6,18 @@
   services.windmill.baseUrl = "https://windmill-507bd7.flakery.xyz/";
   services.windmill.database.urlPath = "/dburl";
   services.postgresql = {
-    authentication = pkgs.lib.mkBefore ''
+    authentication = pkgs.lib.mkForce ''
       host    windmill        windmill        127.0.0.1/32            trust
+      local   all             all                                     trust
+      # IPv4 local connections:
+      host    all             all             127.0.0.1/32            trust
+      # IPv6 local connections:
+      host    all             all             ::1/128                 trust
+      # Allow replication connections from localhost, by a user with the
+      # replication privilege.
+      local   replication     all                                     trust
+      host    replication     all             127.0.0.1/32            trust
+      host    replication     all             ::1/128                 trust
     '';
   };
 }
