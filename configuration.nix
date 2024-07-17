e0b2c3101f6ebb8aa8b3ed776f2cbec7565423a7
rw <rw@jjk.is>
2024-07-17 15:09:26 -0700

    install bash globally

--- configuration.nix
+++ configuration.nix
@@ -2,6 +2,11 @@
 
 {
   networking.firewall.allowedTCPPorts = [ 8001 ];
+
+  environment.systemPackages = with pkgs; [
+    bash
+  ];
+
   system.stateVersion = "23.05";
   services.windmill.enable = true;
   services.windmill.baseUrl = "https://windmill-507bd7.flakery.xyz/";
