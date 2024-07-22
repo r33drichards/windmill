5092a2fdbb7a26a083c88842c24d089a02d0c0bd
rw <rw@jjk.is>
2024-07-22 13:53:36 -0700

    inputs

--- flake.nix
+++ flake.nix
@@ -9,7 +9,7 @@
     
   };
 
-  outputs = { self, nixpkgs, flakery }: {
+  outputs = { self, nixpkgs, flakery, comin }@inputs: {
     nixosConfigurations.flakery = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       modules = [
