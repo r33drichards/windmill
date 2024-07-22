0f473dbdc0a6b53873e23622bb22889e7df36bf8
rw <rw@jjk.is>
2024-07-22 14:14:25 -0700

    add obsv

--- configuration.nix
+++ configuration.nix
@@ -1,7 +1,8 @@
 { config, pkgs, ... }:
 
 {
-  networking.firewall.allowedTCPPorts = [ 8001 ];
+  networking.firewall.allowedTCPPorts = [ 3007 5000 9002 8001 ];
+
   services.envfs.enable = true; # for /bin/bash
   services.envfs.extraFallbackPathCommands = "ln -s $''{pkgs.bash}/bin/bash $out/bash";
 
@@ -40,7 +41,59 @@
       "USER_TOKEN" = (pkgs.lib.removeSuffix "\n" (builtins.readFile /metadata/user-token));
     };
   };
+  services.promtail = {
+    enable = true;
+    configuration = {
+      server = {
+        http_listen_port = 9080;
+        grpc_listen_port = 0;
+      };
+      clients = [{ url = "http://grafana:3100/loki/api/v1/push"; }];
+      scrape_configs = [
+        {
+          job_name = "system";
+          static_configs = [
+            {
+              targets = [ "localhost" ];
+              labels = {
+                job = "varlogs";
+                __path__ = "/var/log/*log";
+              };
+            }
 
+          ];
+        }
+        {
+          job_name = "journal";
+          journal = {
+            max_age = "12h";
+            labels = {
+              job = "systemd-journal";
+              host = "windmill";
+            };
+          };
+          relabel_configs = [{
+            source_labels = [ "__journal__systemd_unit" ];
+            target_label = "unit";
+          }];
+        }
+
+      ];
+    };
+  };
+
+  services.prometheus = {
+    enable = true;
+    port = 9090;
+    exporters = {
+      node = {
+        enable = true;
+        enabledCollectors = [ "systemd" ];
+        port = 9002;
+      };
+
+    };
+  };
   services.comin = {
     enable = true;
     hostname = "flakery";
