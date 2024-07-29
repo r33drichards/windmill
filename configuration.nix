2eb1cd86ee3951be4332cf4b9183e9332339d54e
rw <rw@jjk.is>
2024-07-29 11:33:34 -0700

    not worth(?)

--- configuration.nix
+++ configuration.nix
@@ -143,7 +143,6 @@
       aws s3 cp s3://windmill-fb9bb14a273e85f2/postgresql-backup/windmill.sql.gz /var/backup/postgresql/windmill.sql.gz
       gunzip -f /var/backup/postgresql/windmill.sql.gz
       # exec on db ALTER DATABASE mbcontrol OWNER TO youruser;
-      systemctl stop windmill-worker windmill-worker-native windmill-server.service 
       psql -U postgres -c "DROP DATABASE IF EXISTS windmill"
       psql -U postgres -c "CREATE DATABASE windmill"
       psql -U postgres -c "ALTER DATABASE windmill OWNER TO windmill"
